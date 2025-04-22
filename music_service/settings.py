c5fbdd3348d1a7d58ae887e4b591d4dea88d552e
Kuvarin Gleb <kuvarin2005@mail.ru>
2025-04-22 16:30:08 +0300

    server

--- music_service/settings.py
+++ music_service/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-your-secret-key-here'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['77.91.70.226', 'localhost']
 
 
 # Application definition
